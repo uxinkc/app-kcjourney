a196f88b6fd8aab716dc227ba00da66259a29edd
uxinkc@gmail.com <uxinkc@gmail.com>
2023-02-15 10:02:54 -0600

    Updates

---
+++ assets/useNavigation.c3296d0a.js
@@ -0,0 +1,1 @@
+import{s}from"./index.59aa08e0.js";function u(){const n=s();return{goto:(o,e="")=>{o.includes("http")?e=="_blank"?window.open(o,e):window.location.href=o:n.push(o)}}}export{u};
--- assets/useNavigation.d08854cd.js
+++
@@ -1,1 +0,0 @@
-import{s}from"./index.77ee2b66.js";function u(){const n=s();return{goto:(o,e="")=>{o.includes("http")?e=="_blank"?window.open(o,e):window.location.href=o:n.push(o)}}}export{u};
